7d54e3dea1457679e3cd4fecc2107dd9174df1c9
Akito Michishita <routetheta@gmail.com>
2017-05-03 01:45:11 +0900

    Userのseedを追加

---
+++ db/factories/user.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :dev_user, class: User do
+    sequence(:login) { |i| "user#{i}" }
+    sequence(:mail) { |i| "user#{i}@example.com" }
+
+    sequence(:firstname) { |i| "first#{i}" }
+    sequence(:lastname) { |i| "last#{i}" }
+    password 'password'
+
+    to_create do |instance|
+      instance.save validate: false
+    end
+  end
+end
--- db/seed.rb
+++ db/seed.rb
@@ -11,6 +11,7 @@ console = Logger.new(STDOUT)
 
 console.info 'Delete exist data'
 Project.delete_all
+User.where.not(login: :admin).delete_all
 Member.delete_all
 Version.delete_all
 Issue.delete_all
@@ -19,14 +20,19 @@ console.info 'Create projects'
 create_list :dev_project, 10
 
 console.info 'Create project members'
+admin = User.find_by login: :admin
+users = create_list :dev_user, 5
 Project.all.each do |project|
-  create :dev_member, project: project
+  create :dev_member, project: project, roles: [Role.givable.first], user_id: admin.id
+  users.each do |user|
+    create :dev_member, project: project, user: user
+  end
 end
 
 console.info 'Create versions'
 create_list :dev_version, 20
 
 console.info 'Create issues'
-create_list :dev_issue, 200
+create_list :dev_issue, 500
 
 Setting.notified_events = setting_notified_events
